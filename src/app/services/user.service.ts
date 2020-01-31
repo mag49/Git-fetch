d2242240ac36f65dece07a968d8bdaf1cbf203be
Ngethe <ciru930@gmail.com>
2020-01-31 11:37:23 +0300

    exported the git hub user service client id and secret in the user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UserService {
+  public username:string;
+  public clientid="af59a886926a3c41b772";
+  public clientsecret="a345424c81e1eee3ffc3b2509fc644a0736f5e8c";
+  constructor(private http:HttpClient) { 
+    console.log("service is now ready!");
+    this.username = 'mag49';
+  }
+ getUserInfo(){ 
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+  }
+  getProfileRepos(){ 
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+  }
 
-  constructor() { }
+
+updateProfile(username: string){
+  this.username = username;
 }
+
+}
+
