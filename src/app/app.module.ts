09f2c706c1360179b639d4de27e6eea558c6a50c
Ngethe <ciru930@gmail.com>
2020-01-31 13:11:04 +0300

    addedd bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from'@angular/forms';
-import {userService} from './services/user.service';
+import {UserService} from './services/user.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
